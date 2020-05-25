0d1c68288856fe11539d2e1e90621626b10dcdcb
Virginie <virginie.lenoir17@gmail.com>
2020-05-25 17:14:11 +0200

    finished exo 4

--- script.js
+++ script.js
@@ -21,13 +21,39 @@ footerTag.addEventListener("click", function() {
 // Fonctionnalité 2 :
 // Tu vas faire que si quelqu'un clique sur ce bouton, l'élément HTML portant l'Id navbarHeader perde sa classe collapse. Une fois que ça, ça marche, fait que si on clique à nouveau dessus, la classe collapse soit rajoutée à nouveau à l'élément portant l'Id navbarHeader
 
-let navbarHeader = document.getElementById("navbarHeader");
+let navbarHeader = document.getElementsByClassName("navbar-toggler")[0];
 
-function toggleNavbarId() {
-  console.log("coucou");
-  navbarHeader.classList.toggle("collapse");
-}
+navbarHeader.addEventListener('click', function(){
+  document.getElementById("navbarHeader").classList.toggle("collapse");
+});
 
-document.getElementById("navbarHeader").addEventListener('click', toggleNavbarId);
+// Fonctionnalité 3 :
+// si on clique sur le bouton "Edit" de la première card, le texte de la card va se mettre en rouge de façon irréversible (sauf si on recharge la page)
 
+let firstCardParagraph = document.querySelectorAll("main .card p")[0]
 
+let firstCardEditButton = document.querySelectorAll("main .col-md-4 .btn-group")[0].children[1]
+
+firstCardEditButton.addEventListener("click", function(){
+  firstCardParagraph.style.color = "red"
+});
+
+// Fonctionnalité 4 :
+// si on clique sur le bouton "Edit" de la deuxième card, le texte de la card va se mettre en vert. Si on re-clique dessus, il redevient comme avant ! 
+let secondCardParagraph = document.querySelectorAll("main .card p")[1]
+
+let secondCardEditButton = document.querySelectorAll("main .col-md-4 .btn-group")[1].children[1]
+
+let first_click = true;
+secondCardEditButton.addEventListener("click", function(){
+  if (first_click === true){
+    secondCardParagraph.style.color = "green"
+    first_click = false;
+  } else {
+    secondCardParagraph.style.color = "";
+    first_click = true;
+  }
+});
+
+// Fonctionnalité 5 :
+// si un utilisateur double clique sur la navbar en haut, tout Bootstrap disparaît et la page s'affiche comme si on avait oublié de mettre le CDN qui la relie au fichier CSS. Si possible, rends cette fonctionnalité réversible (un nouveau double-clic fait tout revenir à la normale).
